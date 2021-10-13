8ef5eb4a0f8878e0976138f7316b2906da7fb36d
OnSebii <lang.sebastian.006@gmail.com>
2021-10-13 16:26:58 +0200

    Aufgabe 3

---
+++ client/src/components/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+    <header><slot name="header"></slot></header>
+    <main><slot></slot></main>
+    <footer><slot name="footer"></slot></footer>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+header,
+footer {
+  height: 100px;
+  background-color: green;
+}
+</style>
